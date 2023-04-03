ec9cbc3d46f95656d90be66cff612f143b498f23
Jr-Ocrah <91498437+Jr-Ocrah@users.noreply.github.com>
2023-04-03 23:13:54 +0000

    more classes

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,107 +1,120 @@
-Skip to content
-Search or jump to…
-Pull requests
-Issues
-Codespaces
-Marketplace
-Explore
- 
-@Jr-Ocrah 
-davidinmichael
-/
-alx-higher_level_programming
-Public
-Fork your own copy of davidinmichael/alx-higher_level_programming
-Code
-Issues
-Pull requests
-Actions
-Projects
-Security
-Insights
-alx-higher_level_programming/0x08-python-more_classes/101-nqueens.py /
-@davidinmichael
-davidinmichael I added the other files
-Latest commit 46301f8 13 minutes ago
- History
- 1 contributor
-62 lines (53 sloc)  1.59 KB
- 
-
 #!/usr/bin/python3
-"""
-nqueens backtracking program to print the coordinates of n queens
-on an nxn grid such that they are all in non-attacking positions
-"""
+"""10. N queens"""
+
+import sys
 
 
-from sys import argv
+def create_new_board(n):
+    """Function that creates new nxn sized chessboard with 0's."""
+
+    board = []
+    [board.append([]) for i in range(n)]
+    [row.append(' ') for i in range(n) for row in board]
+    return (board)
+
+
+def copy_board(board):
+    """Function that returns deepcopy of board"""
+
+    if isinstance(board, list):
+        return list(map(copy_board, board))
+
+    return (board)
+
+
+def find_method(board):
+    """Function that finds solution for the Queen"""
+
+    method = []
+    for pos in range(len(board)):
+        for position in range(len(board)):
+            if board[pos][position] == "Q":
+                method.append([pos, position])
+                break
+
+    return (method)
+
+
+def invalid_moves(board, row, col):
+    """Function that shows invalid moves for Queen"""
+
+    for position in range(col + 1, len(board)):
+        board[row][position] = "x"
+
+    for position in range(col - 1, -1, -1):
+        board[row][position] = "x"
+
+    for pos in range(row + 1, len(board)):
+        board[pos][col] = "x"
+
+    for pos in range(row - 1, -1, -1):
+        board[pos][col] = "x"
+
+    c_pos = col + 1
+    for pos in range(row + 1, len(board)):
+        if c_pos >= len(board):
+            break
+
+        board[pos][c_pos] = "x"
+        c_pos += 1
+
+    c_pos = col - 1
+    for pos in range(row - 1, -1, -1):
+        if c_pos < 0:
+            break
+
+        board[pos][c_pos]
+        c_pos -= 1
+
+    c_pos = col + 1
+    for pos in range(row - 1, -1, -1):
+        if c_pos >= len(board):
+            break
+
+        board[pos][c_pos] = "x"
+        c_pos += 1
+
+    c_pos = col - 1
+    for pos in range(row + 1, len(board)):
+        if c_pos < 0:
+            break
+
+        board[pos][c_pos] = "x"
+        c_pos -= 1
+
+
+def correct_method(board, row, queens, solutions):
+    """Function that solves puzzle with recursion"""
+
+    if queens == len(board):
+        solutions.append(find_method(board))
+        return (solutions)
+
+    for position in range(len(board)):
+        if board[row][position] == " ":
+            tmp_board = copy_board(board)
+            tmp_board[row][position] = "Q"
+            invalid_moves(tmp_board, row, position)
+            solutions = correct_method(
+                    tmp_board, row + 1, queens + 1, solutions)
+
+    return (solutions)
+
 
 if __name__ == "__main__":
-    a = []
-    if len(argv) != 2:
+    if len(sys.argv) != 2:
         print("Usage: nqueens N")
-        exit(1)
-    if argv[1].isdigit() is False:
+        sys.exit(1)
+
+    if sys.argv[1].isdigit() is False:
         print("N must be a number")
-        exit(1)
-    n = int(argv[1])
-    if n < 4:
+        sys.exit(1)
+
+    if int(sys.argv[1]) < 4:
         print("N must be at least 4")
-        exit(1)
+        sys.exit(1)
 
-    # initialize the answer list
-    for i in range(n):
-        a.append([i, None])
-
-    def already_exists(y):
-        """check that a queen does not already exist in that y value"""
-        for x in range(n):
-            if y == a[x][1]:
-                return True
-        return False
-
-    def reject(x, y):
-        """determines whether or not to reject the solution"""
-        if (already_exists(y)):
-            return False
-        i = 0
-        while(i < x):
-            if abs(a[i][1] - y) == abs(i - x):
-                return False
-            i += 1
-        return True
-
-    def clear_a(x):
-        """clears the answers from the point of failure on"""
-        for i in range(x, n):
-            a[i][1] = None
-
-    def nqueens(x):
-        """recursive backtracking function to find the solution"""
-        for y in range(n):
-            clear_a(x)
-            if reject(x, y):
-                a[x][1] = y
-                if (x == n - 1):  # accepts the solution
-                    print(a)
-                else:
-                    nqueens(x + 1)  # moves on to next x value to continue
-
-    # start the recursive process at x = 0
-    nqueens(0)
-Footer
-© 2023 GitHub, Inc.
-Footer navigation
-Terms
-Privacy
-Security
-Status
-Docs
-Contact GitHub
-Pricing
-API
-Training
-Blog
-About
-alx-higher_level_programming/101-nqueens.py at main · davidinmichael/alx-higher_level_programming
+    board = create_new_board(int(sys.argv[1]))
+    methods = correct_method(board, 0, 0, [])
+    for sol in methods:
+        print(sol)
